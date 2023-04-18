65416e12ce721b975d7c1bf10a4615e387939ef1
DaryaPopova <pd2405@yandex.ru>
2023-04-18 21:01:45 +0200

    pytest, simple request test, properties

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,19 @@
-def test_1(set_up, some):
-    print('test one')
+import pytest
+import requests
+
+import config
+from tests.conftest import BaseTest
 
 
-def test_2(set_up, some):
-    print('test two')
+class TestMain(BaseTest):
+    def test_1(self, set_up, some):
+        print('test one')
+        url = config.SERVER_HOST + ":" + str(config.SERVER_PORT)
+
+        with requests.get(url) as r:
+            print(r.text)
+
+        self.assertEquals(r.status_code, 200)
+
+    def test_2(self, set_up, some):
+        print('test two')
