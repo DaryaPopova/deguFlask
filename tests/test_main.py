9e94a67a01b22159ebe4c963d56fd3f74adb3476
DaryaPopova <pd2405@yandex.ru>
2023-04-23 19:33:14 +0200

    docker compose

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ from assertpy import assert_that
 
 def test_1(set_up, some):
     print('test one')
-    url = f"{SERVER_HOST}:{SERVER_PORT}/"
+    url = f"http://{SERVER_HOST}:{SERVER_PORT}/"
 
     with requests.get(url) as r:
         assert_that(r.status_code).is_equal_to(200)
@@ -14,7 +14,7 @@ def test_1(set_up, some):
 
 def test_2(set_up, some):
     print('test two')
-    url = f"{SERVER_HOST}:{SERVER_PORT}/user/1"
+    url = f"http://{SERVER_HOST}:{SERVER_PORT}/user/1"
 
     with requests.get(url) as r:
         assert_that(r.status_code).is_equal_to(200)
