61a29273050f15bb78b6eee85bd7d63c0f7edaf2
DaryaPopova <pd2405@yandex.ru>
2023-05-01 18:23:41 +0200

    test of CI

--- tests/test_main.py
+++ tests/test_main.py
@@ -22,12 +22,3 @@ def test_2(set_up, some):
 
     with requests.get(url) as r:
         assert_that(r.status_code).is_equal_to(200)
-
-
-def test_style():
-    style_guide = flake8.get_style_guide(ignore=["E501", "W504", "ANN101", "TYP101", "TYP102"], max_line_length=140)
-    root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    files = [os.path.join(root, "app.py")]
-
-    errors = style_guide.check_files(files)
-    assert_that(errors.total_errors).is_equal_to(0)
