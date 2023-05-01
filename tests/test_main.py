b05533f98a78350dac0e5b9f26feb868a817d854
DaryaPopova <pd2405@yandex.ru>
2023-05-01 18:13:01 +0200

    test of CI

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,7 @@
+import os
+
+from flake8.api import legacy as flake8
+
 from configuration import SERVER_HOST
 from configuration import SERVER_PORT
 import requests
@@ -18,3 +22,12 @@ def test_2(set_up, some):
 
     with requests.get(url) as r:
         assert_that(r.status_code).is_equal_to(200)
+
+
+def test_style():
+    style_guide = flake8.get_style_guide(ignore=["E501", "W504", "ANN101", "TYP101", "TYP102"], max_line_length=140)
+    root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    files = [os.path.join(root, "app.py")]
+
+    errors = style_guide.check_files(files)
+    assert_that(errors.total_errors).is_equal_to(0)
